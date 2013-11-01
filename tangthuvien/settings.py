d338934680a77f6d1f24272b774b188fa58ec2e6
Bang Dao <antipro.vn@gmail.com>
2013-11-01 21:11:48 +0700

    remove dynamic locale middleware

--- tangthuvien/settings.py
+++ tangthuvien/settings.py
@@ -116,7 +116,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'tangthuvien.middleware.ClearTemplateJsCss',
