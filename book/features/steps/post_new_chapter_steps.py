6bad832b47d47930e4cc1803a125ab0667317076
Bang Dao <antipro.vn@gmail.com>
2013-11-04 23:17:41 +0700

    add missing step in post new chapter test

--- book/features/steps/post_new_chapter_steps.py
+++ book/features/steps/post_new_chapter_steps.py
@@ -11,6 +11,10 @@ from book.models.chapter_model import Chapter
 from book.features.steps.favorite_book_steps import find_book_in_list
 from book.features.steps.publish_new_book_steps import when_i_post_a_new_chapter_for_this_book
 
+@step(u'I am reading a book')
+def i_am_reading_a_book(step):
+    i_visit_book_index_page(step)
+    i_read_a_book(step)
 
 @step(u'I submit a new book chapter')
 def i_submit_a_new_book_chapter(step):
@@ -31,7 +35,7 @@ def check_new_chapter_content():
 @step(u'I see new chapter was posted')
 def i_see_new_chapter_was_posted(step):
     find(".notifications").text.should.contain(trans(u"New chapter was posted successfully."))
-    i_read_a_book(step)
+    i_am_reading_a_book(step)
     get_total_chapters(int(world.book_id)).should.be.equal(world.old_total_chapters + 1)
     check_new_chapter_content()
 
