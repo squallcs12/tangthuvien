6b062f87df7df420f108afd9dd589c9533f5d1eb
Bang Dao <bang.dao@eastagile.com>
2013-12-02 15:53:01 +0700

    move generate book prc js to static file

---
+++ book/static/js/book/generate_prc.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+jQuery(window).load(function(){
+	var socket = io.connect('http://localhost:1234');
+	(function($){
+
+		socket.on('generate_book_prc_{{book.id}}', function(data){
+			console.log(data);
+			$("#generate_book_prc_div .progress .progress-bar").addClass('progress-bar-success');
+			$("#generate_book_prc_div .progress .progress-bar").html(LANG_PRC_IS_GENERATED);
+			append_new_book_attachment(data.attachment);
+			setTimeout(function(){
+				$("#generate_book_prc_div .progress").fadeOut();
+			}, 1000);
+		});
+
+		$("#generate_book_prc").click(function(){
+			$("#generate_book_prc_div .progress .progress-bar").html(LANG_PRC_IS_GENERATING);
+			$("#generate_book_prc_div .progress .progress-bar").removeClass('progress-bar-success');
+			$("#generate_book_prc_div .progress").show();
+			socket.emit('generate_book_prc', { book_id: '{{book.id}}' });
+			return false;
+		});
+		$("#generate_book_prc").removeClass('disabled');
+	})(jQuery);
+
+});
