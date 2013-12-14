79d4e4cd4b44bcb0fbd72d6b62072b02e73f917f
Bang Dao <antipro.vn@gmail.com>
2013-12-14 07:25:15 +0700

    change item information in thankshop item factory

--- thankshop/features/factories/__init__.py
+++ thankshop/features/factories/__init__.py
@@ -5,8 +5,8 @@ import factory
 class ItemFactory(factory.Factory):
     FACTORY_FOR = Item
 
-    name = factory.Sequence(lambda n: 'Author {0}'.format(n))
-    short_description = factory.Sequence(lambda n: 'author-{0}'.format(n))
-    long_description = factory.Sequence(lambda n: 'author-{0}'.format(n))
+    name = factory.Sequence(lambda n: 'Item {0}'.format(n))
+    short_description = factory.Sequence(lambda n: 'item-{0}'.format(n))
+    long_description = factory.Sequence(lambda n: 'item-{0}'.format(n))
     price = factory.Sequence(lambda n: n * 100 + 100)
     stocks = factory.Sequence(lambda n: n + 1)
