aeddf5a096a0f971638840c586ec27b08e42f3e2
Bang Dao <antipro.vn@gmail.com>
2013-12-14 07:10:24 +0700

    import item models to thankshop models module

--- thankshop/models/__init__.py
+++ thankshop/models/__init__.py
@@ -1,6 +1,7 @@
 from .user_daily_login_history import UserDailyLoginHistory
 from .thank_point import ThankPoint, ThankPointHistory
 from .package import Package
+from .item import Item
 
 def import_signal():
     from thankshop import signals  # @UnusedImport
