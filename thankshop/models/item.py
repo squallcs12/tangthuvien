e258d9c9e7627b8c932951baa150ab60129083db
Bang Dao <antipro.vn@gmail.com>
2013-12-14 07:07:54 +0700

    add item model for thankshop

---
+++ thankshop/models/item.py
@@ -0,0 +1,17 @@
+'''
+Created on Dec 14, 2013
+
+@author: antipro
+'''
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.TextField()
+    long_description = models.TextField()
+    price = models.IntegerField()
+    stocks = models.IntegerField()
+    image = models.ImageField()
+
+    class Meta:
+        app_label = 'thankshop'
