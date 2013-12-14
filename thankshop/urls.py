d092f658e365960e682af2076ff7faa7478fc34f
Bang Dao <antipro.vn@gmail.com>
2013-12-14 22:24:07 +0700

    change view acording to the renaming of view file

--- thankshop/urls.py
+++ thankshop/urls.py
@@ -7,9 +7,9 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('thankshop.views',
-    url(r'^thank_point/buy/return', 'thank_point_shop.paypal_return', name='thankshop_thank_point_paypal_return'),
-    url(r'^thank_point/buy/cancel', 'thank_point_shop.paypal_cancel', name='thankshop_thank_point_paypal_cancel'),
-    url(r'^thank_point/buy/(?P<package_id>\d*)', 'thank_point_shop.buy', name='thankshop_thank_point_buy'),
-    url(r'^thank_point/', 'thank_point_shop.index', name='thankshop_thank_point_shop'),
-    url(r'^$', 'shop_homepage.index', name='thankshop_shop_homepage'),
+    url(r'^thank_point/buy/return', 'thank_point_shop_view.paypal_return', name='thankshop_thank_point_paypal_return'),
+    url(r'^thank_point/buy/cancel', 'thank_point_shop_view.paypal_cancel', name='thankshop_thank_point_paypal_cancel'),
+    url(r'^thank_point/buy/(?P<package_id>\d*)', 'thank_point_shop_view.buy', name='thankshop_thank_point_buy'),
+    url(r'^thank_point/', 'thank_point_shop_view.index', name='thankshop_thank_point_shop'),
+    url(r'^$', 'shop_homepage_view.index', name='thankshop_shop_homepage'),
 )
