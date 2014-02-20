e2c81a0b13e843c75b7b3d752a5f250eabee92aa
Bang Dao <bang.dao@eastagile.com>
2014-02-20 16:17:58 +0700

    fix deploy script

--- fabfile.py
+++ fabfile.py
@@ -31,7 +31,7 @@ class Deploy(object):
 
     @classmethod
     def sudo(cls, command):
-        sudo(command)
+        return sudo(command)
 
     @classmethod
     def init(cls):
@@ -72,6 +72,7 @@ class Deploy(object):
         sudo("mkdir -p %s/log" % cls.share_dir)
         sudo("mkdir -p %s/log/copybook" % cls.share_dir)
         sudo("mkdir -p %s/media" % cls.share_dir)
+        sudo("mkdir -p %s/node_modules" % cls.share_dir)
         sudo("mkdir -p %s/media/uploads" % cls.share_dir)
         sudo("mkdir -p %s/media/uploads/ckeditor" % cls.share_dir)
         sudo("mkdir -p %s/media/thumbs" % cls.share_dir)
@@ -202,7 +203,7 @@ class Deploy(object):
         sudo("rm %s" % cls.current_dir)
         sudo("ln -s %s %s" % (cls.release_dir, cls.current_dir))
 
-        sudo("chmod 777 %s/bin/gunicorn_start.sh" % cls.release_dir)
+        sudo("chmod 777 %s/bin/*.sh" % cls.release_dir)
 
         cls.sudo_virtualenv("supervisorctl -c%s/bin/supervisor.conf shutdown" % cls.current_dir)
         cls.sudo_virtualenv("supervisord -c%s/bin/supervisor.conf" % cls.current_dir)
@@ -293,6 +294,12 @@ class Deploy(object):
             sudo("%s install socket.io" % path)
             sudo("%s install express" % path)
 
+    @classmethod
+    def install_node_modules(cls):
+        packages = cls.sudo("cat %s/socket.io/requirements.txt" % cls.release_dir).split("\n")
+        for package in packages:
+            cls.sudo("cd %s; npm install %s" % (cls.share_dir, package))
+        cls.sudo("ln -s %s/node_modules %s/node_modules" % (cls.share_dir, cls.release_dir))
 
     @classmethod
     def setup(cls):
@@ -319,6 +326,7 @@ class Deploy(object):
         current_time = run("date +%Y%m%d%H%M%S")
         cls.checkout_source(current_time)
         cls.install_requirements()
+        cls.install_node_modules()
         cls.run_migration()
         cls.collect_statics()
         cls.combine_django_messages()
