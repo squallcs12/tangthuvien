c832e07bd9bc5e0f301215b5e7ae7900f96a8c18
Bang Dao <bang.dao@eastagile.com>
2014-02-24 10:57:46 +0700

    Finish admin page for manage user thank points

--- thankshop/admin/__init__.py
+++ thankshop/admin/__init__.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from thankshop import models
+from thankshop.admin import thankpoint_admin
 
 admin.site.register(models.Package)
-admin.site.register(models.ThankPoint)
+admin.site.register(models.ThankPoint, thankpoint_admin.ThankpointAdmin)
 admin.site.register(models.Item)
