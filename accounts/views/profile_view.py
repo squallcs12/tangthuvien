f879312abb0691c4753d572633653eef91548097
Bang Dao <bang.dao@eastagile.com>
2014-02-26 13:07:52 +0700

    remove wrong commit in profile

--- accounts/views/profile_view.py
+++ accounts/views/profile_view.py
@@ -4,8 +4,6 @@ Created on Sep 18, 2013
 @author: antipro
 '''
 from django.template.response import TemplateResponse
-from django.contrib import messages
 def main(request, template="accounts/profile.phtml"):
     data = {}
-    messages.success(request, "thankshop/daily_thankpoints_increase.phtml", extra_tags="template")
     return TemplateResponse(request, template, data)
