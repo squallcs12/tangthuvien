1d019b1a97b7a155f74dd85f2be73a94ee68ac2a
Bang Dao <bang.dao@eastagile.com>
2014-03-03 11:48:40 +0700

    replace tab by spaces

--- tangthuvien/static/js/foot_define.js
+++ tangthuvien/static/js/foot_define.js
@@ -1,32 +1,32 @@
 (function($){
-	$(".onetime-notification[data-dismiss='alert']").click(function(){
-		var key = $(this).data('key');
+    $(".onetime-notification[data-dismiss='alert']").click(function(){
+        var key = $(this).data('key');
 
-		request = {};
-		request['url'] = ONETIME_NOTIFICATION_SUBMIT_URL;
-		request['type'] = 'post';
-		request['data'] = {};
-		request['data']['key'] = key;
+        request = {};
+        request['url'] = ONETIME_NOTIFICATION_SUBMIT_URL;
+        request['type'] = 'post';
+        request['data'] = {};
+        request['data']['key'] = key;
 
-		$.ajax(request);
-	});
+        $.ajax(request);
+    });
 
-	$("body").on("shown.bs.modal", "div.modal.modal-centered", function(){
-		$(this).css("top", jQuery(window).height() / 2 - $(this).height() / 2);
-	});
-	$("body").on("hidden.bs.modal", "div.modal.modal-centered", function(){
-		$(this).css("top", "");
-	});
+    $("body").on("shown.bs.modal", "div.modal.modal-centered", function(){
+        $(this).css("top", jQuery(window).height() / 2 - $(this).height() / 2);
+    });
+    $("body").on("hidden.bs.modal", "div.modal.modal-centered", function(){
+        $(this).css("top", "");
+    });
 
-	$.fn.showLoading = function(){
-		$(this).each(function(){
-			$(this).append(LOADING_IMAGE_HTML);
-		});
-	};
+    $.fn.showLoading = function(){
+        $(this).each(function(){
+            $(this).append(LOADING_IMAGE_HTML);
+        });
+    };
 
-	$.fn.removeLoading = function(){
-		$(this).each(function(){
-			$(".loading", this).remove();
-		});
-	};
+    $.fn.removeLoading = function(){
+        $(this).each(function(){
+            $(".loading", this).remove();
+        });
+    };
 })(jQuery);
--- tangthuvien/static/js/head_define.js
+++ tangthuvien/static/js/head_define.js
@@ -1,31 +1,31 @@
 function csrfSafeMethod(method) {
-	// these HTTP methods do not require CSRF protection
-	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
 function sameOrigin(url) {
-	// test that a given url is a same-origin URL
-	// url could be relative or scheme relative or absolute
-	var host = document.location.host;
-	// host + port
-	var protocol = document.location.protocol;
-	var sr_origin = '//' + host;
-	var origin = protocol + sr_origin;
-	// Allow absolute or scheme relative URLs to same origin
-	return (url == origin || url.slice(0, origin.length + 1) == origin + '/')
-			|| (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin
-					+ '/') ||
-			// or any other URL that isn't scheme relative or absolute i.e
-			// relative.
-			!(/^(\/\/|http:|https:).*/.test(url));
+    // test that a given url is a same-origin URL
+    // url could be relative or scheme relative or absolute
+    var host = document.location.host;
+    // host + port
+    var protocol = document.location.protocol;
+    var sr_origin = '//' + host;
+    var origin = protocol + sr_origin;
+    // Allow absolute or scheme relative URLs to same origin
+    return (url == origin || url.slice(0, origin.length + 1) == origin + '/')
+            || (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin
+                    + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e
+            // relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
 }
 
 jQuery.ajaxSetup({
-	beforeSend : function(xhr, settings) {
-		if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-			xhr.setRequestHeader("X-CSRFToken", jQuery.cookie('csrftoken'));
-		}
-	}
+    beforeSend : function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+            xhr.setRequestHeader("X-CSRFToken", jQuery.cookie('csrftoken'));
+        }
+    }
 });
 function get_real_link(link){
     var a = document.createElement('a');
@@ -35,8 +35,8 @@ function get_real_link(link){
     return link;
 }
 jQuery(document).on("click", "ul.pagination li.disabled a", function(){
-	return false;
+    return false;
 });
 jQuery(document).on("click", "ul.pagination li.active a", function(){
-	return false;
+    return false;
 });
