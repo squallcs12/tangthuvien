301a6a198e649346affbe4bbe47658011dc002ec
Bang Dao <bang.dao@eastagile.com>
2014-03-03 12:14:33 +0700

    change ajax csrf by using $.ajaxPrefilter

--- tangthuvien/static/js/head_define.js
+++ tangthuvien/static/js/head_define.js
@@ -12,31 +12,35 @@ function sameOrigin(url) {
     var sr_origin = '//' + host;
     var origin = protocol + sr_origin;
     // Allow absolute or scheme relative URLs to same origin
-    return (url == origin || url.slice(0, origin.length + 1) == origin + '/')
-            || (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin
-                    + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e
-            // relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
+    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') || (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+    // or any other URL that isn't scheme relative or absolute i.e
+    // relative.
+    !(/^(\/\/|http:|https:).*/.test(url));
 }
 
-jQuery.ajaxSetup({
-    beforeSend : function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-            xhr.setRequestHeader("X-CSRFToken", jQuery.cookie('csrftoken'));
+(function($) {
+    $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+        if (!csrfSafeMethod(options.type) && sameOrigin(options.url)) {
+            if(typeof(options.headers) == 'undefined'){
+                options.headers = {};
+            }
+            options.headers["X-CSRFToken"] = $.cookie('csrftoken');
         }
-    }
-});
-function get_real_link(link){
+    });
+})(jQuery);
+
+function get_real_link(link) {
     var a = document.createElement('a');
     a.href = link;
     link = a.href;
     delete a;
     return link;
 }
-jQuery(document).on("click", "ul.pagination li.disabled a", function(){
+
+
+jQuery(document).on("click", "ul.pagination li.disabled a", function() {
     return false;
 });
-jQuery(document).on("click", "ul.pagination li.active a", function(){
+jQuery(document).on("click", "ul.pagination li.active a", function() {
     return false;
 });
