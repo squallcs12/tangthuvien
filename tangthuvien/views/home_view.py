9810b72aa2d6ff0f73338361dcc087cea14967db
Bang Dao <daotranbang@gmail.com>
2014-04-12 11:58:21 +0700

    fix typo error

--- tangthuvien/views/home_view.py
+++ tangthuvien/views/home_view.py
@@ -15,10 +15,8 @@ def main(request, template="homepage.phtml"):
     data['show_top_banner'] = True
     for time_settings in settings.HOMEPAGE_RECENT_BOOK_UPDATE_TIME:
     	last_update_time = datetime.datetime.now() - datetime.timedelta(**time_settings)
-    	data['books'] = Book.objects
-    							.filter(last_update__gte=last_update_time,
-    									chapters_count__gt=0)
-    							.order_by('-last_update')
+    	data['books'] = Book.objects.filter(last_update__gte=last_update_time,
+    									chapters_count__gt=0).order_by('-last_update')
     	if data['books'].count():
     		break
 
