e2d64e7d71b2bda3502a8b32118cce734e6f03d7
Bang Dao <antipro.vn@gmail.com>
2014-04-27 07:37:38 +0700

    login required for book reading config view

--- book/views/book_reading_config_view.py
+++ book/views/book_reading_config_view.py
@@ -5,7 +5,11 @@ Created on Oct 26, 2013
 '''
 from book.forms import ConfigReadingSectionForm
 from django.http.response import HttpResponse
+from decorator.ajax_required_decorator import ajax_required
+from django.contrib.auth.decorators import login_required
 
+@ajax_required
+@login_required
 def ajax(request):
     form = ConfigReadingSectionForm(request.user, request.POST)
     if form.is_valid():
