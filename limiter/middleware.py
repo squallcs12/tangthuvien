1df5c6cc4165490c4652e882859d50b4b0f8f7d5
Bang Dao <antipro.vn@gmail.com>
2014-04-27 20:34:07 +0700

    on error, limiter return HttpResponseForbidden

--- limiter/middleware.py
+++ limiter/middleware.py
@@ -4,12 +4,12 @@ Created on Apr 21, 2014
 @author: eastagile
 '''
 from limiter.exceptions import LimiterException
-from django.http.response import HttpResponse
+from django.http.response import HttpResponseForbidden
 import json
 
 class Limiter(object):
     def process_exception(self, request, exception):
         if isinstance(exception, LimiterException):
-            response = HttpResponse(status=400)
+            response = HttpResponseForbidden(())
             response['messages'] = json.dumps({"error": [exception.message]})
             return response
