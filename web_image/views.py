90da082ca33aad2ee15a04c3f359d65c56523f43
Bang Dao <antipro.vn@gmail.com>
2014-05-11 16:28:25 +0700

    delete old file before generate new image

--- web_image/views.py
+++ web_image/views.py
@@ -17,9 +17,11 @@ class WebImageGenerateView(View):
 
         import os
         program = settings.realpath('program/wkhtmltoimage')
-        os.system("%s \"%s\" %s.jpg" % (program, url, os.path.join(
-                                                        settings.MEDIA_ROOT,
-                                                         "web_images",
-                                                         filename)))
+        des_file = "%s.jpg" % os.path.join(settings.MEDIA_ROOT, "web_images", filename)
+        try:
+            os.unlink(des_file)
+        except OSError:
+            pass
+        os.system("%s \"%s\" %s" % (program, url, des_file))
 
         return HttpResponse("1")
